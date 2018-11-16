694910405e2f4807821d6f0c353847babc3b9687
hapiny <tmironov6@gmail.com>
2018-11-16 23:27:22 +0300

    mongoDb added.

--- app.js
+++ app.js
@@ -2,29 +2,48 @@ var express = require("express");
 var app = express();
 var request = require("request");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+var campgroundScheme = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+});
+
+var Campground = mongoose.model("Campground", campgroundScheme);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Grantie Hill", image: "https://cdn.travelpulse.com/images/99999999-9999-9999-9999-999999999999/777BB633-D2CE-56A8-E06C-7C2F8E6CD135/630x355.jpeg"},
-    {name: "Mountain Goat's Rest", image: "https://cdn.vox-cdn.com/thumbor/-JoPdcgAuLTUsWiDZ62CX4wb33k=/0x0:5225x3479/1200x800/filters:focal(2195x1322:3031x2158)/cdn.vox-cdn.com/uploads/chorus_image/image/54137643/camping_tents.0.jpg"},
-];
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-    res.render("campgrounds", {campgrounds: campgrounds} );
+    Campground.find({}, function(err, campgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: campgrounds} );
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var newCamp = {name: name, image: image};
-    campgrounds.push(newCamp);
+    Campground.create(newCamp, function(err, camp) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("ADDED NEW CAMPGROUND:");
+            console.log(camp);
+        }
+    });
     res.redirect("/campgrounds");
 });
 
